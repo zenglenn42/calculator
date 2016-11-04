3db4ca5b6a4523a718e999fe8149d3bcaac12475
zenglen42 <gsnospampls@pobox.com>
2016-11-04 11:58:19 -0500

    implement calc guts

--- assets/javascript/calc.js
+++ assets/javascript/calc.js
@@ -11,39 +11,61 @@ Simple webapp calculator.
 //	calc.num.push(1);  num = [1];
 //	calc.num.push(0);  num = [1, 0];
 //
-//	calc.setOperator("+");; // joinedNum = 10; num = [];
+//	calc.setOperator("plus");; // joinedNum = 10; num = [];
 //
 //	calc.num.push(2);	// num = [2];
 //	calc.num.push(3);	// num = [2, 3];
 //
-//	calc.run(); // joinedNum = 23;
+//	result = calc.run(); // joinedNum = 23;
 //				// result = operand[0] [operator] operand[1]
-//
-//	display calc.result;
 //	
-//    calc.operators.push(1);
-//	  calc.operators.push(2);
-//    calc.operand = "+";
-//    var result = calc.run();
 
 var calc {
 	operators: ["+", "-", "/", "^"],
-	operator: [],
+	operator: "";
 	num: [];
+	num1: 0;
+	num2: 0;
 	operand: "",  /* character */
 	result:0,
+	init: function() {
+		this.result = 0;
+		this.operand = "";
+		this.operators = [];
+		this.num = [];
+		this.num1 = 0;
+		this.num2 = 0;
+	},
 	run: function() {
-		return this.result;
+		this.num2 = toNum(this.num);
+		switch(operator) {
+			case "plus":
+				this.result = this.num1 + this.num2;
+				break;
+			case "minus":
+				this.result = this.num1 - this.num2;
+				break;
+			case "times":
+				this.result = this.num1 * this.num2;
+				break;
+			case "divide":
+				this.result = this.num1 / this.num2;
+				break;
+			case "power":
+				this.result = Math.pow(this.num1, this.num2);
+				break;
+			default {
+				console.log("Illegal operator: ", operator);
+			}
+		}
 	}
 	setOperator: function(operator) {
-		this.operator = operator;
-		// convert num array to number
+		this.operator = strOperator;
+		this.num1 = toNum(this.num);
 	}
 }
 
-function initCalc() {
-	calc.result = 0;
-	calc.operand = "";
-	calc.operators = [];
-	calc.num = [];
+function toNum(numArray) {
+	var numStr = numArray.join("");
+	num = parseInt(numStr);
 }
